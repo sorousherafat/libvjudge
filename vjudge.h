b63e78cb4eb531bac12728cf3bf127141c3e5ac0
Mehrad <mehrad_milanloo@yahoo.com>
2023-09-24 21:47:45 +0330

    Modify: added  struct

--- vjudge.h
+++ vjudge.h
@@ -10,6 +10,7 @@
 #define VJUDGE_MAX_NAME_SIZE 128
 #define VJUDGE_MAX_ASSERTIONS_NO 1024
 #define VJUDGE_MAX_TESTS_NO 64
+#define VJUDGE_MAX_SRC_FILES_NO 32
 
 typedef enum {
     NO_ERROR = 0,
@@ -48,5 +49,13 @@ typedef struct {
     test_t tests[VJUDGE_MAX_TESTS_NO];
     error_t error;
 } judge_result_t;
+
+typedef struct {
+    char *test_dir_path;
+    size_t src_files_count;
+    char *src_file_paths[VJUDGE_MAX_SRC_FILES_NO];
+} judge_input_t;
+
+
 
 #endif // VJUDGE_VJUDGE_H
